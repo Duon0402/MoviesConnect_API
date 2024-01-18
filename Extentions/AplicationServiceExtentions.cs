6f19404a604ca8a1054bc5eeb52c6db53fbda8b4
Duon0402 <duongdangtruong.it@gmail.com>
2024-01-19 00:37:34 +0700

    connect db

---
+++ Extentions/AplicationServiceExtentions.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extentions
+{
+    public static class AplicationServiceExtentions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
