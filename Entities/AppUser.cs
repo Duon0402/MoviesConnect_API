22492bba8305d05dd537251b6ba9bbf46ab94f64
Duon0402 <duongdangtruong.it@gmail.com>
2024-01-20 01:03:15 +0700

    add-migration

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -2,7 +2,14 @@
 
 namespace API.Entities
 {
-    public class AppUser : IdentityUser
+    public class AppUser : IdentityUser<int>
     {
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+
+        // role
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
